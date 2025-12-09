a94545d2d356d860cc2e9b96dff493675303341d
VapaaLassi <117110618+VapaaLassi@users.noreply.github.com>
2025-12-09 13:02:33 +0200

    Mocked data text works.

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,50 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function RegionText({ dataRow }) {
+  const all = dataRow.all;
+  const foreign = dataRow.foreign;
+  var percentage = new Intl.NumberFormat('default', {
+    style: 'percent',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(foreign / all);;
+
+  return (
+    <p>
+      {dataRow.kunta}n {all}:sta varhaiskasvatukseen osallistuneesta lapsesta {foreign} ({percentage}) oli vieraskielisiä vuonna 2024.
+    </p>);
+}
+
+function FilteredRegion({ languages }) {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <h1>Päiväkotien kielitaustakone</h1>
+      <div>
+        <RegionText
+          dataRow={languages[0]} />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
-  )
+  );
 }
+
+function App() {
+  return <FilteredRegion languages={LANGUAGES} />;
+}
+
+const LANGUAGES = [
+  { kunta: "Akaa", all: 552, foreign: 14 },
+  { kunta: "Espoo", all: 16928, foreign: 4959 },
+  { kunta: "Helsinki", all: 30870, foreign: 7313 },
+  { kunta: "Forssa", all: 529, foreign: 105 },
+  { kunta: "Hämeenlinna", all: 2700, foreign: 308 },
+  { kunta: "Kempele", all: 1331, foreign: 6 },
+];
+
+const FOREIGN_AVG = 13.91;
 
 export default App
