ba91b266538104ff6aada7cb327e710eddacde07
VapaaLassi <117110618+VapaaLassi@users.noreply.github.com>
2025-12-09 16:23:04 +0200

    Restored config.

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/jobtest/',
+  base: /jobtest/,
   plugins: [
     react({
       babel: {
